4fd5d96cc96bbe332a3c80fe97086f6e59869d6e
Programador5 <programador5@sypsolutions.com>
2022-08-30 13:01:59 -0500

    Agregar archivos de proyecto.

---
+++ ChatAspNetSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAspNetSignalR.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task EnviarMensaje(string user, string mensaje)
+    {
+        await Clients.All.SendAsync("RecibirMensaje", user, mensaje);
+    }
+}
---
+++ ChatAspNetSignalR/wwwroot/js/app/chat/chat.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+
+var conexion = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+conexion.on("RecibirMensaje", function (user, mensaje) {
+    var li = document.createElement("li");
+    li.textContent = user + "-" + mensaje;
+    document.getElementById("lstMensajes").appendChild(li);
+})
+
+conexion.start().then(function () {
+    var li = document.createElement("li");
+    li.textContent = "Inicio";
+    document.getElementById("lstMensajes").appendChild(li);
+}).catch(function (error) {
+    console.error(error);
+})
+
+document.getElementById("btnEnviar").addEventListener("click", function (event) {
+    event.preventDefault();
+    var user = document.getElementById("txtUsuario").value;
+    var mensaje = document.getElementById("txtMensaje").value;
+
+    conexion.invoke("EnviarMensaje", user, mensaje)
+        .catch(function (error) {
+            console.error(error);
+        });
+})
